2e9a25ed591bdc0b1306af25cd31fc2373037223
Marcos Cano Miranda <marcoscano@1288796.no-reply.drupal.org>
2016-06-01 10:54:41 +0200

    Add settings form.

--- src/Form/RolechangenotifySettingsForm.php
+++ src/Form/RolechangenotifySettingsForm.php
@@ -37,46 +37,33 @@ class RolechangenotifySettingsForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
-    $config = $this->config('role_change_notify.settings');
+    $roles = user_roles(TRUE);
+    unset($roles[\Drupal\user\RoleInterface::AUTHENTICATED_ID]);
 
-//    $form['role_change_notify_enabled_nodetypes'] = array(
-//      '#title' => $this->t('Enabled Node Types'),
-//      '#description' => $this->t('Check node types enabled for Previous/Next'),
-//      '#type' => 'checkboxes',
-//      '#options' => node_type_get_names(),
-//      '#default_value' => $config->get('role_change_notify_enabled_nodetypes'),
-//    );
+    if (empty($roles)) {
+      $form['noroles'] = array(
+        '#markup' => '<p><em>' . t("No roles have been set up except Authenticated User. \nPlease set up additional roles if you want to use role notification.") . '</em></p>',
+      );
+      return parent::buildForm($form, $form_state);
+    }
+
+    $config = $this->config('role_change_notify.settings');
+    $form['instructions'] = array(
+      '#markup' => '<p><strong>' . $this->t('Select roles for which notification should be sent:') . '</strong></p>',
+    );
+    foreach ($roles as $roleid => $roleobject) {
+      $form["role_change_notify_{$roleid}"] = array(
+        '#type' => 'checkbox',
+        '#title' => $roleobject->get('label'),
+        '#default_value' => $config->get("role_change_notify_{$roleid}"),
+      );
+    }
+    $account_page_link = \Drupal\Core\Link::createFromRoute($this->t('Account settings'), 'entity.user.admin_form')->toString();
+    $form['settings_info'] = array(
+      '#markup' => '<p>' . t('E-mail content (subject and body text) can be configured on the !account_settings_link page.', array('!account_settings_link' => $account_page_link)) . '</p>',
+    );
 
     return parent::buildForm($form, $form_state);
-
-
-    /**
-     * @FIXME lo siguiente es el ejemplo de D7 para este form:
-     */
-//    $roles = user_roles(TRUE);
-//
-//    unset($roles[DRUPAL_AUTHENTICATED_RID]);
-//    if (sizeof($roles) == 0) {
-//      $form['noroles'] = array(
-//        '#markup' => '<p><em>' . t("No roles have been set up except Authenticated User. \nPlease set up additional roles if you want to use role notification.") . '</em></p>',
-//      );
-//      return $form;
-//    }
-//    $form['instructions'] = array(
-//      '#markup' => '<p><strong>' . t('Select roles for which notification should be sent:') . '</strong></p>',
-//    );
-//    foreach ($roles as $roleid => $rolename) {
-//      $form["role_change_notify_{$roleid}"] = array(
-//        '#type' => 'checkbox',
-//        '#title' => $rolename,
-//        '#default_value' => variable_get("role_change_notify_{$roleid}", FALSE),
-//      );
-//    }
-//    $form['settings_info'] = array(
-//      '#markup' => '<p>' . t('E-mail content (subject and body text) can be configured on the !account_settings_link page.', array('!account_settings_link' => l(t('Account settings'), 'admin/config/people/accounts', array('fragment' => 'edit-role-change-notify')))) . '</p>',
-//    );
-//
-//    return system_settings_form($form);
 
   }
 
@@ -85,9 +72,11 @@ class RolechangenotifySettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // Save the config values.
-    $this->config('role_change_notify.settings')
-//      ->set('role_change_notify_enabled_nodetypes', $form_state->getValue('role_change_notify_enabled_nodetypes'))
-      ->save();
+    $config = $this->config('role_change_notify.settings');
+    foreach ($form_state->getValues() as $key => $value) {
+      $config->set($key, $value);
+    }
+    $config->save();
     parent::submitForm($form, $form_state);
   }
 
